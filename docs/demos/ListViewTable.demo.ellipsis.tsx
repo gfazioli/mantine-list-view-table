f31a52d053bd8696284edb1f568de0fdc6c0ff7c
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2026-03-20 16:10:28 +0100

    🛠️ refactor(docs): add Table.ScrollContainer and column widths to demos
    - Wrap ListViewTable in Table.ScrollContainer for horizontal scrolling
    - Add explicit width to columns for consistent sizing
    - Update column titles to reflect ellipsis behavior
    - Remove unused onColumnResize callback from demos
    - Sync demo code snippets with new Table imports

--- docs/demos/ListViewTable.demo.ellipsis.tsx
+++ docs/demos/ListViewTable.demo.ellipsis.tsx
@@ -1,4 +1,5 @@
 import { ListViewTable, type ListViewTableColumn } from '@gfazioli/mantine-list-view-table';
+import { Table } from '@mantine/core';
 import { MantineDemo } from '@mantinex/demo';
 import { columnsEllipsis, columnsEllipsisCode } from './columns-ellipsis';
 import { dataEllipsis, dataEllipsisCode } from './data-ellipsis';
@@ -8,41 +9,39 @@ const columns: ListViewTableColumn[] = columnsEllipsis;
 
 function Demo() {
   return (
-    <ListViewTable
-      columns={columns}
-      data={data}
-      rowKey="id"
-      withTableBorder
-      withColumnBorders
-      highlightOnHover
-      enableColumnResizing
-      onColumnResize={(columnKey, width) => {
-        // eslint-disable-next-line no-console
-        console.log(`Column '${columnKey}' resized to: ${width}px`);
-      }}
-    />
+    <Table.ScrollContainer minWidth={500}>
+      <ListViewTable
+        columns={columns}
+        data={data}
+        rowKey="id"
+        withTableBorder
+        withColumnBorders
+        highlightOnHover
+        enableColumnResizing
+      />
+    </Table.ScrollContainer>
   );
 }
 
 const code = `
 import { ListViewTable } from '@gfazioli/mantine-list-view-table';
+import { Table } from '@mantine/core';
 import { data } from './data';
 import { columns } from './columns';
 
 function Demo() {
   return (
-    <ListViewTable
-      columns={columns}
-      data={data}
-      rowKey="id"
-      withTableBorder
-      withColumnBorders
-      highlightOnHover
-      enableColumnResizing
-      onColumnResize={(columnKey, width) => {
-        console.log(\`Column '\${columnKey}' resized to: \${width}px\`);
-      }}
-    />
+    <Table.ScrollContainer minWidth={500}>
+      <ListViewTable
+        columns={columns}
+        data={data}
+        rowKey="id"
+        withTableBorder
+        withColumnBorders
+        highlightOnHover
+        enableColumnResizing
+      />
+    </Table.ScrollContainer>
   );
 }
 `;
